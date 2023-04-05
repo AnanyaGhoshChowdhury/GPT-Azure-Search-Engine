1918d4c49b3f0db535e3b6853af069fa4f0a5b8b
Ananya Ghosh Chowdhury <ananyag@microsoft.com>
2023-04-04 18:04:35 -0700

    credentials update

--- app/credentials.py
+++ app/credentials.py
@@ -7,12 +7,13 @@ AZURE_OPENAI_API_VERSION = "2023-03-15-preview"
 
 # Change these below with your own services credentials
 
-AZURE_SEARCH_ENDPOINT = "Enter your Azure Cognitive Search Endpoint ..."
-AZURE_SEARCH_KEY = "Enter your Azure Cognitive Search Primary Key ..."
-COG_SERVICES_NAME = "Enter your Azure Cognitive services multi-service account name ..."
-COG_SERVICES_KEY = "Enter your Azure Cognitive services multi-service Key ..."
-AZURE_OPENAI_ENDPOINT = "Enter your Azure OpenAI Endpoint ..."
-AZURE_OPENAI_KEY = "Enter your Azure OpenAI Key ..."
+AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-bvkk6jb3or5oc.search.windows.net"
+AZURE_SEARCH_KEY = "dAuXCoEXt4N4x8LQ4V4Jf4eM3VI4is0etxu9XghVVqAzSeBSLMeq"
+COG_SERVICES_NAME = "cognitive-service-bvkk6jb3or5oc"
+COG_SERVICES_KEY = "101f86f5d12a4ccf8d5da6b49ae82ea8"
+AZURE_OPENAI_ENDPOINT = "https://openaiplayinstance.openai.azure.com/"
+AZURE_OPENAI_KEY = "a7d014a49c3d4aa796644d84fdbeadb6"
+
 
 
 
