4580a8ae1756f04c9967207d979338305447492b
parthshiras <parthshiras@gmail.com>
2023-04-11 19:32:08 -0400

    Update credentials.py

--- app/credentials.py
+++ app/credentials.py
@@ -1,20 +1,22 @@
 
 # We recommend leaving these two variables as below. The DATASOURCE_CONNECTION_STRING contains 9800 PDFs from Arxiv Computer Science publications. Change it if you want to use your own data.
-DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=demodatasetspananya;AccountKey=Bakef0mz+YPLHmW5BdXinncXv+a676yckVDfzT5SdEBEdZRYi1f5pcBNHsPb412qG9Mk2Ye+FQg9+ASt3AfEWA==;EndpointSuffix=core.windows.net"
+DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=cog-search-demo;AccountKey=SAN+/n0g3BzUZOppso6LOAMkmGz4OX9p+bQsKik9rCe0NYSouUaBQLkWf9g2xxZPTyoL75qVlrQ2+AStjarFOw==;EndpointSuffix=core.windows.net"
 AZURE_SEARCH_API_VERSION = '2021-04-30-Preview'
 AZURE_OPENAI_API_VERSION = "2023-03-15-preview"
 
 
 # Change these below with your own services credentials
 
-AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-bvkk6jb3or5oc.search.windows.net"
-AZURE_SEARCH_KEY = "dAuXCoEXt4N4x8LQ4V4Jf4eM3VI4is0etxu9XghVVqAzSeBSLMeq"
-COG_SERVICES_NAME = "cognitive-service-bvkk6jb3or5oc"
-COG_SERVICES_KEY = "101f86f5d12a4ccf8d5da6b49ae82ea8"
-AZURE_OPENAI_ENDPOINT = "https://openaiplayinstance.openai.azure.com/"
-AZURE_OPENAI_KEY = "a7d014a49c3d4aa796644d84fdbeadb6"
+AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-lvep5t5fsu77m.search.windows.net"
+AZURE_SEARCH_KEY = "fKie71bxOTqLGt0wkrz7NaUqNtVPHIlQbyeo8LjfamAzSeA4a5rN"
+COG_SERVICES_NAME = "cognitive-service-lvep5t5fsu77m"
+COG_SERVICES_KEY = "8e891f938bc54ea28623d609f7d8b75e"
+AZURE_OPENAI_ENDPOINT = "https://openai-testing-parthshiras.openai.azure.com/"
+AZURE_OPENAI_KEY = "590d2ec5b7bc4e9aa0cc212ab7ad601f"
 
 
 
 
 
+# DefaultEndpointsProtocol=https;AccountName=demodatasetspananya;AccountKey=Bakef0mz+YPLHmW5BdXinncXv+a676yckVDfzT5SdEBEdZRYi1f5pcBNHsPb412qG9Mk2Ye+FQg9+ASt3AfEWA==;EndpointSuffix=core.windows.net
+# DefaultEndpointsProtocol=https;AccountName=cog-search-demo;AccountKey=SAN+/n0g3BzUZOppso6LOAMkmGz4OX9p+bQsKik9rCe0NYSouUaBQLkWf9g2xxZPTyoL75qVlrQ2+AStjarFOw==;EndpointSuffix=core.windows.net
