976a6d933e9e6901047aa629e1d30e0d8bf5444c
parthshiras <parthshiras@gmail.com>
2023-04-12 00:03:16 -0400

    Update sidebar.py

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -12,9 +12,9 @@ def sidebar():
             GPT Smart Search allows you to ask questions about your
             documents and get accurate answers with instant citations.
             
-            This engine finds information from ~9.8k [Computer Science Publications in Arxiv from 2020-2022](https://www.kaggle.com/datasets/1b6883fb66c5e7f67c697c2547022cc04c9ee98c3742f9a4d6c671b4f4eda591)
+            This engine finds information from CSA-DA Wiki document base [Wiki](https://dev.azure.com/csadevadvocates/Documentation/_wiki/wikis/Documentation.wiki/15/Welcome)
             
-            [Github Repo](https://github.com/pablomarin/GPT-Azure-Search-Engine/)
+            [Github Repo](https://github.com/parthshiras/GPT-Azure-Search-Engine/)
         """
         )
         st.markdown("---")
